21cbd27eed108f809cfc032a75a3d1761098049b
Andre M. de Souza <andresouza44@gmail.com>
2023-02-15 18:41:56 -0300

    Ex 06 Fatorial

--- exercicios-array-loop/src/Ex5_Tabuada.java
+++ exercicios-array-loop/src/Ex5_Tabuada.java
@@ -4,6 +4,11 @@ public class Ex5_Tabuada {
     public static void main(String[] args) {
         Scanner  scan = new Scanner(System.in);
         int tabuada;
-               
+        System.out.println("Qual a tabuada deseja ver ? ");       
+        tabuada = scan.nextInt();
+        for (int x=1; x<11; x++){
+            System.out.println(tabuada+ " X "+ x + " = " + (tabuada*x) );
+
+        }
     }
 }
---
+++ exercicios-array-loop/src/Ex6_Fatorial.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+/*
+ * Calcular o Fatorial de um Número informado pelo usuário
+ */
+
+public class Ex6_Fatorial {
+  public static void main(String[] args) {
+    int numero; 
+    int fatorial = 1;
+    
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Digite um número: ");
+    numero = scan.nextInt();
+    for (int x = numero; x>=1; x--){
+        fatorial = fatorial*x;
+    }
+    System.out.println(numero+"! = "+fatorial);
+
+    
+
+    }
+   
+}
