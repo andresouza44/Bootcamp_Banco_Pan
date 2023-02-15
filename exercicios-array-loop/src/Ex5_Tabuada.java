3ec0e4bfc1a1194abc61da07a9b2ec360afff20a
Andre M. de Souza <andresouza44@gmail.com>
2023-02-15 17:44:52 -0300

    Exercício 5: Tabuada

---
+++ exercicios-array-loop/src/Ex5_Tabuada.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Ex5_Tabuada {
+    public static void main(String[] args) {
+        Scanner  scan = new Scanner(System.in);
+        int tabuada;
+               
+    }
+}
